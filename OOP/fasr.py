7314bb66e53d0810480251a0d5ee9f245bc9f3db
Ravi Goswami <raviroa25@gmail.com>
2026-07-02 22:34:28 +0530

    Add initial implementation of Dog class and sales data analysis script

---
+++ OOP/fasr.py
@@ -0,0 +1,22 @@
+class Dog:
+     def __init__(self , name , color):
+          self.name = name
+          self.color = color
+
+
+D1 = Dog("vicky","white")
+
+print(D1.name)
+print(D1.color)
+
+
+# class Dog:
+#     def __init__(self, name, breed):
+#         self.name = name
+#         self.breed = breed
+
+# # Create dog objects - using positional arguments
+# dog1 = Dog("Buddy", "Golden Retriever")
+# dog2 = Dog("Max", "Beagle")
+
+# print(dog1)
